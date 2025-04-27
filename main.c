71ec54236568c91567f9b1daf496097567cb5526
Oussama Elhassouni <oelhasso@c1r4p9.1337.ma>
2025-04-27 19:40:07 +0100

    focus, that appears non stop on the terminal

---
+++ main.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void f()
+{
+    printf("\n");
+    printf("███████\n");
+    printf("██     \n");
+    printf("███████\n");
+    printf("██     \n");
+    printf("██     \n");
+    printf("██     \n");
+    printf("\n");
+    return ;
+}
+
+void o(){
+    printf("\n");
+    printf("███████      \n");
+    printf("██           ██████     \n");
+    printf("███████    ██      ██\n");
+    printf("██         ██      ██\n");
+    printf("██         ██      ██\n");
+    printf("██           ██████\n");
+    printf("\n");
+    return;
+}
+
+void c(){
+    printf("\n");
+    printf("\n");
+    printf("              ██████        ██████\n");
+    printf("            ██      ██    ██      \n");
+    printf("            ██      ██    ██      \n");
+    printf("            ██      ██    ██      \n");
+    printf("              ██████        ██████\n");
+    printf("\n");
+    return;
+}
+
+void u(){
+    printf("\n");
+    printf("\n");
+    printf("                            ██████    ██      ██\n");
+    printf("                          ██          ██      ██\n");
+    printf("                          ██          ██      ██\n");
+    printf("                          ██          ██      ██\n");
+    printf("                            ██████      ██████\n");
+    printf("\n");
+    return;
+}
+
+void s(){
+    printf("\n");
+    printf("\n");
+    printf("                                      ██      ██      ███████\n");
+    printf("                                      ██      ██    ██       \n");
+    printf("                                      ██      ██      ██████ \n");
+    printf("                                      ██      ██           ██\n");
+    printf("                                        ██████      ███████  \n");
+    printf("\n");
+    return ;
+}
+
+void ss(){
+    printf("\n");
+    printf("                                                            \n");
+    printf("                                                    ██████       \n");
+    printf("                                                  ██ \n");
+    printf("                                                    ███████\n");
+    printf("                                                           ██\n");
+    printf("                                                    ███████\n");
+    printf("\n");
+    return ;
+}
+
+int main ()
+{
+    int i;
+    system("clear");
+    while (1)
+    {
+        f();
+        i = 0;
+        while (i < 150000000)
+            i++;
+        system("clear");
+        o();
+        i = 0;
+        while (i < 150000000)
+            i++;
+        system("clear");
+        c();
+        i = 0;
+        while (i < 150000000)
+            i++;
+        system("clear");
+        u();
+        i = 0;
+        while (i < 150000000)
+            i++;
+        system("clear");
+        s();
+        i = 0;
+        while (i < 150000000)
+            i++;
+        system("clear");
+        ss();
+        i = 0;
+        while (i < 150000000)
+            i++;
+        system("clear");
+    }
+}
